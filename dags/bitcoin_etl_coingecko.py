03fac59c3c06ecc638cb03fa33be76b7fc6fa4cf
fabiokp <fabiokpaim@gmail.com>
2025-09-27 21:58:33 -0300

    Update start_date in bitcoin_etl_coingecko.py

--- dags/bitcoin_etl_coingecko.py
+++ dags/bitcoin_etl_coingecko.py
@@ -100,7 +100,7 @@ def fetch_bitcoin_history_from_coingecko():
 @dag(
     default_args=DEFAULT_ARGS,
     schedule="0 0 * * *",  # diário à 00:00 UTC
-    start_date=pendulum.datetime(2025, 9, 17, tz="UTC"),
+    start_date=pendulum.datetime(2025, 9, 27, tz="UTC"),
     catchup=True,
     max_active_runs=1,
     concurrency=1,
